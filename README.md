3974ccb868d1c1444b6c7d69c3365b8d1c255dd2
Kim Desrosiers <kimdesro@gmail.com>
2018-04-22 21:58:09 -0400

    Add Cargo.lock to ignored files. Add a little documentation.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Factory_Steel
 [![Build Status](https://travis-ci.org/kimond/factory_steel.svg?branch=master)](https://travis-ci.org/kimond/factory_steel)
 
-Factory-steel is a fixture replacement greatly inspired by [factory_bot](https://github.com/thoughtbot/factory_bot)
+Factory_steel is a fixture replacement greatly inspired by [factory_bot](https://github.com/thoughtbot/factory_bot)
 and [factory_boy](https://github.com/FactoryBoy/factory_boy). Currently it is an experimental project. 
 Although usable, it is still very limited in term of features and reliability.
 
--- factory_steel/src/lib.rs
+++ factory_steel/src/lib.rs
@@ -1,8 +1,20 @@
+//! # Factory_steel
+//! Factory_steel is a fixture replacement greatly inspired by [factory_bot](https://github.com/thoughtbot/factory_bot)
+//! and [factory_boy](https://github.com/FactoryBoy/factory_boy). Currently it is an experimental project.
+
+#[allow(unused_imports)]
 #[macro_use]
 extern crate factory_steel_derive;
 
+#[doc(hidden)]
 pub use factory_steel_derive::*;
 
+
+/// A generic factory
+///
+/// This trait is automatically implemented using the `factory` derive.
+/// It is also possible to implement it manually if you want to define custom behaviors for
+/// nested factories.
 pub trait Factory {
     fn create() -> Self;
 }
